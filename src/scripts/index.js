a5083a900bf859070ddac3a190855e42e0cf704b
Andrey <dvij@iMac.local>
2020-03-02 18:03:36 +0200

    add datePicker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import { renderCalendalBlock } from "./renderBloks.js";
 import {} from "./createEvent.js";
 import { instance } from "./events.js";
+import { picker } from "./intervalTimeLine.js";
 
 document.addEventListener("DOMContentLoaded", function() {
   renderCalendalBlock();
