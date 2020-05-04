48d43ab11fecce51a7e1d1c5a37d69f391bedc18
Andrey <dvij@iMac.local>
2020-05-04 16:12:58 +0300

    fix

--- src/scripts/renderEvents.js
+++ src/scripts/renderEvents.js
@@ -10,11 +10,11 @@ export const renderEvents = () => {
   });
   const filteredEvents = [];
   weekDays.forEach(el => {
-    events.forEach(event => {
+    events.forEach((event, index) => {
       if (event.day == el.getAttribute("data-set-day") && event.month == el.getAttribute("data-set-month") && event.year == el.getAttribute("data-set-year")) {
         filteredEvents.push(event)
         el.querySelector(`.calendar__sector-line[data-set-hour="${event.eventTimeFrom.substr(0, 3)}00"]`).innerHTML = `
-        <div style="height: ${event.timeLengthInMinutes}px; margin-top: ${event.eventTimeFrom.substr(3)}px;" class="event" data-id="${event.id}">
+        <div style="height: ${event.timeLengthInMinutes}px; margin-top: ${event.eventTimeFrom.substr(3)}px;" class="event" data-id="${event.id}" data-index="${index}">
           <h3 class="event-title">${event.title}</h3>
           <div class="event-time"><span class="event-timeFrom">${event.eventTimeFrom}</span> - <span class="event-timeTo">${event.eventTimeTo}</span></div>
         </div>
