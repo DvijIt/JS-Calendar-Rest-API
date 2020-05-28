91875d51034ba5753602e0a100f75a0020bac0e2
Andrey <dvij@iMac.local>
2020-05-28 20:23:13 +0300

    calendar webpack add loaders

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {} from "./scripts/renderCalendar.js";
+import {} from "./scripts/saveEvent.js";
+import {} from "./scripts/touchEvent.js";
+import {} from "./scripts/deleteEvent.js";
+import {renderCalendar} from "./scripts/renderCalendar.js";
+import './style.scss'
+
+document.addEventListener('DOMContentLoaded', renderCalendar)
--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import {} from "./renderCalendar.js";
-import {} from "./saveEvent.js";
-import {} from "./touchEvent.js";
-import {} from "./deleteEvent.js";
-
-import {renderCalendar} from "./renderCalendar.js";
-
-document.addEventListener('DOMContentLoaded', renderCalendar)
